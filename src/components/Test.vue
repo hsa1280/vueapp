fc857ac71a6f1b0ae203424ba399a8e5d563cf57
Huang <shian.huang@verizon.com>
2017-03-17 16:30:01 -0700

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title" /> <br/>
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <ul>
+        <li v-for="item in items">
+            {{item.title}}
+        </li>
+    </ul>
+    <button v-on:click="greet('hello world')">Say Greeting</button>
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data() {
+      return {
+        title: 'Hello World',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        },
+        showName: false,
+        items: [
+          {title: 'item one'},
+          {title: 'item two'},
+          {title: 'item three'}
+        ]
+      }
+    },
+    methods: {
+      greet: function(greeting) {
+        alert(greeting)
+      },
+      pressKey: function(e) {
+        console.log(e.target.value)
+      },
+      enterHit: function() {
+        console.log('you hit enter')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
